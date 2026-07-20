9e82d3e4770d8b5f3722cfb7d1f618ab8995b45e
Debabrata24 <mr.debabrtapc2006@gmail.com>
2026-07-20 17:28:25 +0530

    solidity

--- soli.sol
+++ soli.sol
@@ -1,3 +1,4 @@
+
 // 1. We tell the compiler what license we're using. It's just a good habit.
 // SPDX-License-Identifier: MIT
 
@@ -8,16 +9,22 @@ pragma solidity ^0.8.20;
 contract HelloWorld {
     // 4. This is a "State Variable". It is data stored permanently
     //    with the contract on the blockchain.
-    string public greeting = "Hello, World!fef";
-    uint  mynumber=30;
+    string public greeting = "Hello, World!fef"; ///// this are store in permanet storage 
+    uint mynumber = 30;
     //public means, axceble by public function call
-    int a =-20;
-    // suggestion also availble 
+    int a = -20;
+    // suggestion also availble
 
-    uint public  myNumber=10;
-    function setMynumner(uint _myNumber ) public {
-      myNumber = _myNumber;
+    uint public myNumber = 10;
+    function setMynumner(uint _myNumber) public { ///// this are, all function  store in tempuraray  storage 
+        myNumber = _myNumber;
     }
+    string public  myStatus="dev";
 
-
+    function setMystatus(string memory _myStatus) public {
+        myStatus = _myStatus;
+        // for string need to write memory here  also  you can use calldata 
+        //for read only data use calldata 
+        //and for read plus write , use memory
+    }
 }
